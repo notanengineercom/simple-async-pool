0e478007cd2d92bbe0dee1dfa53f295ac4b9623b
Enrique Pöhlmann <45039845+notanengineercom@users.noreply.github.com>
2022-07-14 18:16:52 +0200

    extract options from index

---
+++ src/options.ts
@@ -0,0 +1,16 @@
+import { TFunction } from './types'
+
+interface PoolOptions {
+  output?: 'AsyncIterator' | 'Promise'
+  concurrency?: number
+}
+type DecideOnPoolOptions<T, P> = P extends PoolOptions ? P['output'] extends 'AsyncIterator' ? AsyncIterableIterator<T> : Promise<T[]> : Promise<T[]>
+
+const isPoolOptions = (value: unknown): value is PoolOptions => typeof value === 'object'
+const getPoolOptions = (poolOptions?: PoolOptions | TFunction<any[], unknown>): Required<PoolOptions> => {
+  const pp = isPoolOptions(poolOptions) ? poolOptions : undefined
+  const defaultPoolOptions: Required<PoolOptions> = { output: 'Promise', concurrency: 1 }
+  return { ...defaultPoolOptions, ...pp }
+}
+
+export { PoolOptions, DecideOnPoolOptions, isPoolOptions, getPoolOptions }
